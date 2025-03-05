c3c78ce67f234158ee8fe239ecc37c185a8a3f19
Ayaan Khan <khanayaan.2003@gmail.com>
2025-03-05 17:30:28 +0500

    TESTING Q2

--- Task.py
+++ Task.py
@@ -6,4 +6,17 @@ print("This was added to main and feature branch isnt updated yet")
 
 print("Testiing")
 
-print (THIS IS A BUG)
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    debug_mode = os.getenv('DEBUG', 'False')
+    return f"Hello, World! Debug mode is {debug_mode}"
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    debug_mode = os.getenv('DEBUG')
+    app.run(host='0.0.0.0', port=port, debug=(debug_mode == 'True'))
